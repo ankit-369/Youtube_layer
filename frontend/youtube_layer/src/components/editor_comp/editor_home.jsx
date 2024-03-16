e836802522c8940c48b2e36dc3e8d16e421cb20b
ankit-369 <ankitkuchara420@gmail.com>
2024-03-16 21:10:06 +0530

    create editor dashboard

--- frontend/youtube_layer/src/components/editor_comp/editor_home.jsx
+++ frontend/youtube_layer/src/components/editor_comp/editor_home.jsx
@@ -111,7 +111,7 @@ const Editor_home = () => {
         <div className="col-span-2 ">
           <div className="flex flex-col gap-4">
             <Popup trigger={<button className="inline-flex bg-gray-700 text-white items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 px-8 rounded-full">Add Video</button>} position="right center">
-              <div>Popup content here !!</div>
+              <div>this is popup</div>
             </Popup>
           </div>
         </div>
